7f894d608cf848700c5c4e1c7eb4596c2c9324ab
Ingrid Stevens <ingrid.stevens@students.uni-freiburg.de>
2022-06-21 21:54:09 +0200

    Create challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,24 @@
+def solution(x):
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+
+#   List the entered encrypted code phrase as an array
+    inputArray = list(x)
+    
+#   Define our solution Array (with reversed letters)
+    solutionArray = []
+    
+#   Check for any non-lowercase characters, 
+    for count, letter in enumerate(inputArray):
+        if letter in alphabet:
+            position_in_array = alphabet.index(inputArray[count])
+            opposite_position = len(alphabet) - position_in_array - 1
+            opposite_letter = alphabet[opposite_position]
+
+            solutionArray.append(opposite_letter)
+        else: 
+            solutionArray.append(letter)
+
+    return ''.join(solutionArray)
+
+       
+# To run, enter: solution("wrw blf hvv ozhg mrtsg'h vkrhlwv?")
